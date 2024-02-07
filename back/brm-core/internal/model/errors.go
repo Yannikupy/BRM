02250c72a3aff0d140cae54e4633481640618f97
papey08 <popov.m4tvei@yandex.ru>
2024-02-07 20:38:26 +0300

    [backend/brm-core] add ErrServiceError

--- back/brm-core/internal/model/errors.go
+++ back/brm-core/internal/model/errors.go
@@ -6,5 +6,7 @@ var ErrCompanyNotExists = errors.New("company with required id does not exist")
 var ErrEmployeeNotExists = errors.New("employee with required id does not exist")
 var ErrContactNotExists = errors.New("contact with required id does not exist")
 
-var ErrDatabaseError = errors.New("something wrong with the database")
 var ErrAuthorization = errors.New("no rights to make operation: ownerId mismatched")
+
+var ErrDatabaseError = errors.New("something wrong with the database")
+var ErrServiceError = errors.New("something wrong with the server")
