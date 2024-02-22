be5875e1ebc85be21626ae56e85659c65fb23977
papey08 <popov.m4tvei@yandex.ru>
2024-02-22 18:18:20 +0300

    [back/transport-api] update app interface & models

--- back/transport-api/internal/app/app.go
+++ back/transport-api/internal/app/app.go
@@ -1,11 +1,13 @@
 package app
 
 import (
+	"transport-api/internal/adapters/grpcads"
 	"transport-api/internal/adapters/grpccore"
 )
 
 type appImpl struct {
 	core grpccore.CoreClient
+	ads  grpcads.AdsClient
 }
 
 func NewApp(coreCli grpccore.CoreClient) App {
