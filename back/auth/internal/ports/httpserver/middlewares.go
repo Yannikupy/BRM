04e54a2521f425dac8cff31ba302d36b934e6284
papey08 <popov.m4tvei@yandex.ru>
2024-03-04 01:46:37 +0300

    [frontend] update CORS in auth service

--- back/auth/internal/ports/httpserver/middlewares.go
+++ back/auth/internal/ports/httpserver/middlewares.go
@@ -49,17 +49,3 @@ func panicMiddleware(logs logger.Logger) gin.HandlerFunc {
 		c.Next()
 	}
 }
-
-func corsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Methods", "*")
-		c.Header("Access-Control-Allow-Headers", "*")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		if c.Request.Method == http.MethodOptions {
-			c.AbortWithStatus(http.StatusOK)
-			return
-		}
-		c.Next()
-	}
-}
--- back/auth/internal/ports/httpserver/server.go
+++ back/auth/internal/ports/httpserver/server.go
@@ -3,9 +3,11 @@ package httpserver
 import (
 	"auth/internal/app"
 	"auth/pkg/logger"
+	"github.com/gin-contrib/cors"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +15,15 @@ import (
 func New(addr string, a app.App, logs logger.Logger) *http.Server {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.New()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:4200"},
+		AllowMethods:     []string{"POST"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
