9b70c0679045d185700368b30f4b19f563145414
papey08 <popov.m4tvei@yandex.ru>
2024-03-05 03:06:08 +0300

    [back/brm-leads] add repo interface

---
+++ back/brm-leads/internal/repo/repo_interface.go
@@ -0,0 +1,17 @@
+package repo
+
+import (
+	"brm-leads/internal/model"
+	"context"
+)
+
+type LeadsRepo interface {
+	GetLeadById(ctx context.Context, id uint64) (model.Lead, error)
+	GetLeads(ctx context.Context, companyId uint64, filter model.Filter) ([]model.Lead, error)
+	CreateLead(ctx context.Context, lead model.Lead) (model.Lead, error)
+	UpdateLead(ctx context.Context, id uint64, upd model.UpdateLead) (model.Lead, error)
+	DeleteLead(ctx context.Context, id uint64) error
+
+	GetStatuses(ctx context.Context) (map[string]uint64, error)
+	GetStatusById(ctx context.Context, id uint64) (string, error)
+}
