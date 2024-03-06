187b5fc5d82952e6535ed58ed784a5e9e8dbe498
papey08 <popov.m4tvei@yandex.ru>
2024-03-06 15:58:58 +0300

    [back/brm-leads] add leads support to transporo api

--- back/transport-api/internal/ports/httpserver/leads/presenters.go
+++ back/transport-api/internal/ports/httpserver/leads/presenters.go
@@ -1,18 +1,9 @@
 package leads
 
-type addLeadRequest struct {
-	ProducerId    int    `json:"producer_id"`
-	ConsumerId    int    `json:"consumer_id"`
-	Title         string `json:"title"`
-	Text          string `json:"text"`
-	Price         int    `json:"price"`
-	ResponsibleId int    `json:"responsible_id"`
-}
-
 type updateLeadRequest struct {
-	Title         string `json:"title"`
-	Text          string `json:"text"`
-	Price         int    `json:"price"`
-	ResponsibleId int    `json:"responsible_id"`
-	Status        int    `json:"status"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Price       uint   `json:"price"`
+	Status      uint64 `json:"status"`
+	Responsible uint64 `json:"responsible"`
 }
