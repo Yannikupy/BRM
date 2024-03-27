93a7bfa08d159d3c8da55f3344966d7e9e5ddcf8
papey08 <popov.m4tvei@yandex.ru>
2024-03-27 19:16:07 +0300

    [back/notifications] implement grpc interface

--- back/notifications/internal/model/errors.go
+++ back/notifications/internal/model/errors.go
@@ -9,6 +9,7 @@ var (
 	ErrNotificationNotFound      = errors.New("notification with required id does not exist")
 	ErrWrongNotificationType     = errors.New("found notification with wrong type")
 	ErrClosedLeadAlreadyAnswered = errors.New("notification of closed lead with required id already answered")
+	ErrCompanyNotFound           = errors.New("company with required id does not exist")
 
 	ErrDatabaseError = errors.New("something wrong with notifications database")
 	ErrStatsError    = errors.New("something wrong with stats service")
