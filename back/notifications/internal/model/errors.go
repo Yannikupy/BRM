d15a27ea255de2efa17cec81b14c38221d4a6f95
papey08 <popov.m4tvei@yandex.ru>
2024-03-27 15:02:18 +0300

    [back/notifications] update model

--- back/notifications/internal/model/errors.go
+++ back/notifications/internal/model/errors.go
@@ -1,1 +1,15 @@
 package model
+
+import "errors"
+
+var (
+	ErrInvalidInput = errors.New("given data is invalid")
+
+	ErrPermissionDenied          = errors.New("no rights to make this operation")
+	ErrNotificationNotFount      = errors.New("notification with required id does not exist")
+	ErrClosedLeadAlreadyAnswered = errors.New("notification of closed lead with required id already answered")
+
+	ErrDatabaseError = errors.New("something wrong with notifications database")
+	ErrStatsError    = errors.New("something wrong with stats service")
+	ErrServiceError  = errors.New("something wrong with notifications service")
+)
--- back/notifications/internal/model/notifications/closed_lead.go
+++ back/notifications/internal/model/notifications/closed_lead.go
@@ -3,4 +3,5 @@ package notifications
 type ClosedLead struct {
 	AdId            uint64
 	ProducerCompany uint64
+	Answered        bool
 }
