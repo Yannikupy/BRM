412878f671d07e4f177ca2327f7290956ac51b72
Yannikupy <yan26.202@gmail.com>
2024-03-31 22:01:21 +0300

    ads

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,14 +4,17 @@ import {LoginComponent} from './login/login.component';
 import {authGuard} from './guards/auth.guard';
 import {RegisterComponent} from './register/register.component';
 import {CompanyComponent} from "./company/company.component";
+import {AdsComponent} from "./ads/ads.component";
 
 export const routes: Routes = [
+  {path: '', redirectTo: 'company', pathMatch:"full"},
   {
     path: 'contacts',
     component: ContactsComponent,
     canActivate: [authGuard],
   },
   {path: 'company', component: CompanyComponent, canActivate: [authGuard]},
+  {path: 'ads', component: AdsComponent, canActivate: [authGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'sign-up', component: RegisterComponent},
 ];
--- frontend/src/app/left-menu/left-menu.component.ts
+++ frontend/src/app/left-menu/left-menu.component.ts
@@ -1,8 +1,9 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {MatListModule} from '@angular/material/list';
 import {RouterModule} from '@angular/router';
 import {DalService} from "../DAL/core/dal.service";
 import {AuthService} from "../services/auth.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-left-menu',
@@ -11,14 +12,23 @@ import {AuthService} from "../services/auth.service";
   templateUrl: './left-menu.component.html',
   styleUrl: './left-menu.component.scss',
 })
-export class LeftMenuComponent {
+export class LeftMenuComponent implements OnChanges {
+  @Input() employeeId!: number
 
   dalService = inject(DalService);
   authService = inject(AuthService);
+
+  subscription = new Subscription()
+
   employeeName: string = ''
 
   constructor() {
-    this.dalService.getEmployeeById(+this.authService.currentUserDataSig()?.
-      ["employee-id"]!).subscribe(value => this.employeeName = `${value.data.second_name} ${value.data.first_name}`)
+    this.subscription.add(this.dalService.getEmployeeById(+this.authService.currentUserDataSig()?.
+      ["employee-id"]!).subscribe(value => this.employeeName = `${value.data.second_name} ${value.data.first_name}`))
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.subscription.add(this.dalService.getEmployeeById(+this.authService.currentUserDataSig()?.
+      ["employee-id"]!).subscribe(value => this.employeeName = `${value.data.second_name} ${value.data.first_name}`))
+    }
 }
